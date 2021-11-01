792ff0013133f8f55c06dec4578ab86438540442
BichoTraveco <77287281+BichoTraveco@users.noreply.github.com>
2021-11-01 17:24:27 +0000

    Update insert-inputs.js

--- Base de Dados/Trabalho 1/insert-inputs.js
+++ Base de Dados/Trabalho 1/insert-inputs.js
@@ -1,4 +1,4 @@
-const name = "Guilherme";
+let name = "Guilherme";
 let arr = ['Albânia', 'Alemanha', 'Andorra', 'Áustria', 'Bélgica', 'Bielorrúsia', 'Bósnia e Herzegovina', 'Bulgária', 'Cazaquistão', 'Chipre', 'Croácia', 'Dinamarca', 'Eslováquia', 'Eslovênia', 'Espanha', 'Estônia', 'Finlândia', 'França', 'Grécia', 'Hungria', 'Irlanda', 'Islândia', 'Itália', 'Letônia', 'Liechtenstein', 'Lituânia', 'Luxemburgo', 'Malta', 'Moldávia', 'Mônaco', 'Montenegro', 'Noruega', 'Países Baixos', 'Polónia', 'Portugal', 'Checoslováquia', 'Macedónia do Norte', 'Inglaterra', 'Escócia', 'Irlanda do Norte', 'País de Gales', 'Roménia', 'Rússia', 'San Marino', 'Sérvia', 'Suécia', 'Suíça', 'Turquia', 'Ucrânia', 'Vaticano'];
 for (let i in arr) {
     console.log("INSERT INTO gostade VALUES('"+name+"','"+arr[i]+"');");
